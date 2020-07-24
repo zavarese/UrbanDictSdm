8cce4d994978918f210a4ad38b1612ed95ece607
zavarese <53958165+zavarese@users.noreply.github.com>
2020-07-24 20:01:01 -0300

    Key App

--- app/src/main/java/br/edu/ifsp/scl/urbandictsdm/model/UrbanDictionaryApi.kt
+++ app/src/main/java/br/edu/ifsp/scl/urbandictsdm/model/UrbanDictionaryApi.kt
@@ -3,7 +3,7 @@ package br.edu.ifsp.scl.urbandictsdm.model
 
 object UrbanDictionaryApi {
     val URL_BASE = "https://mashape-community-urban-dictionary.p.rapidapi.com/"
-    val END_POINT = "define?term=" // Não é bem um endpoint, mas...
+    val END_POINT = "define?term=" // Não é bem um endpoint, mas.....
     val X_RAPIDAPI_KEY_FIELD = "x-rapidapi-key"
     val X_RAPIDAPI_KEY_VALUE = "1e239d7265msh135287f52f4c375p1b8453jsn7bb921bcb9a8"
     val X_RAPIDAPI_HOST_FIELD = "x-rapidapi-host"
